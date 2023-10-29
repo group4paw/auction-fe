aae752cd25405d5c10ab265c9b886ee754e615fa
Farhan Franaka <farhanfranaka0505@gmail.com>
2023-10-30 06:39:36 +0700

    Membuat simple Login app sebagai template awal

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useState } from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <button type="submit">Login</button>
+    </div>
+  );
+};
+
+export default LoginPage;
