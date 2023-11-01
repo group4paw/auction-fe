16e12e85e4cc98903ae0cabcfe05c231583434d5
Farhan Franaka <farhanfranaka0505@gmail.com>
2023-11-02 00:00:30 +0700

    Menyesuaikan Config Font, Color, dan Typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,63 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    fontFamily: {
+      sarala: ["var(--sarala-font)"],
+      staatliches: ["var(--staatliches-font)"],
+    },
+    colors: {
+      blue: {
+        100: "#7CACF3",
+        300: "#4488EE",
+        500: "#1565DF",
+        700: "#1155BB",
+        900: "#1F3382",
+      },
+      teal: {
+        100: "#9AFBF5",
+        300: "#5DF9FE",
+        500: "#1CF6FE",
+        700: "#01D8DF",
+        900: "#019DA2",
+      },
+      pink: {
+        100: "#FE9AC7",
+        300: "#FE5DA5",
+        500: "#FE298A",
+        700: "#FE0B78",
+        900: "#CB015C",
+      },
+      alert: {
+        red: "#CA3D5A",
+        green: "#B1E873",
+      },
+      neutral: {
+        100: "#F0F0F0",
+        300: "#DDEAFE",
+        500: "#C4D8FD",
+        700: "#4C76A1",
+        900: "#385472",
+      },
+      shade: {
+        100: "#2C3036",
+        300: "#23252C",
+        500: "#090A0A",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
