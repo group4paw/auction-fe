c5aa8368c866de69c2db0670105455312ee7c664
hisazakura <luthfanarrayyan@gmail.com>
2023-11-09 21:27:24 +0700

    Updated Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
-import PreviewCard from "@/components/PreviewCard"; 
+import PreviewCard from "@/components/PreviewCard";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home | Easybid",
-  description: "...",
+	title: "Home | Easybid",
+	description: "...",
 };
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <PreviewCard/>
-		    <PreviewCard/>
-		    <PreviewCard/>
-      </div>
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex lg:flex-wrap lg:gap-y-10">
+				<PreviewCard />
+				<PreviewCard />
+				<PreviewCard />
+				<PreviewCard />
+				<PreviewCard />
+				<PreviewCard />
+			</div>
+		</main>
+	);
 }
