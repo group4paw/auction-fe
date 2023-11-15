0860fd1f573b22053d5c7c54e4bb59e49405178d
hisazakura <luthfanarrayyan@gmail.com>
2023-11-15 14:05:57 +0700

    Refactor wishlist auctions

--- src/components/Buy/Wishlist/ListCard.tsx
+++ src/components/Buy/Wishlist/ListCard.tsx
@@ -3,89 +3,73 @@
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import Card from "@/components/Buy/Home/Card";
-import { useSelector } from "react-redux";
 
 export default function ListCard() {
-  const [fetchStatus, setFetchStatus] = useState(false);
-  const [listAuction, setListAuction] = useState([] as any[]);
-  const [isLoading, setIsLoading] = useState(false);
+	const [fetchStatus, setFetchStatus] = useState(false);
+	const [listAuction, setListAuction] = useState([] as any[]);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const [user, setUser] = React.useState({} as any);
+	const [user, setUser] = React.useState({} as any);
 
-  useEffect(() => {
-    if (!localStorage.getItem("user")) {
-      setFetchStatus(true);
-    } else {
-      setUser(JSON.parse(localStorage.getItem("user") || "{}"));
-      let userId = JSON.parse(localStorage.getItem("user") || "{}")._id;
-      if (!fetchStatus) {
-        fetchData(userId);
-        setFetchStatus(true);
-      }
-    }
-  }, []);
+	useEffect(() => {
+		if (!localStorage.getItem("user")) {
+			setFetchStatus(true);
+		} else {
+			setUser(JSON.parse(localStorage.getItem("user") || "{}"));
+			let userId = JSON.parse(localStorage.getItem("user") || "{}")._id;
+			if (!fetchStatus) {
+				fetchData(userId);
+				setFetchStatus(true);
+			}
+		}
+	}, [fetchStatus]);
 
-  const fetchData = async (userId: any) => {
-    setIsLoading(true);
-    setListAuction([]);
-    let dataTemp = [] as any[];
-    let wishlist = [] as any[];
-    try {
-      await axios
-        .get("https://auction-api-4.vercel.app/auction/")
-        .then((res) => {
-          dataTemp = res.data;
-        });
+	const fetchData = async (userId: any) => {
+		setIsLoading(true);
+		setListAuction([]);
+		let auctions = [] as any[];
+		let wishlist = [] as any[];
+		try {
+			await axios.get("https://auction-api-4.vercel.app/auction/").then((res) => {
+				auctions = res.data;
+			});
 
-      await axios
-        .get("https://auction-api-4.vercel.app/wishlist/" + userId)
-        .then((res) => {
-          wishlist = res.data;
-        });
+			await axios.get("https://auction-api-4.vercel.app/wishlist/" + userId).then((res) => {
+				wishlist = res.data;
+			});
 
-      dataTemp.forEach((item: any) => {
-        let isWishlist = false;
-        wishlist.forEach((wish: any) => {
-          if (item._id == wish.idAuction) {
-            isWishlist = true;
-          }
-        });
-        item.isWishlist = isWishlist;
-      });
+			wishlist = wishlist.map(item => item.idAuction);
+			auctions = auctions.filter(auction => wishlist.includes(auction._id));
 
-      dataTemp = dataTemp.filter((item: any) => item.isWishlist == true);
+			setListAuction(auctions);
 
-      setListAuction(dataTemp);
+			setIsLoading(false);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-      setIsLoading(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  return (
-    <>
-      {!isLoading ? (
-        listAuction.length > 0 ? (
-          listAuction.map((data, index) => {
-            return (
-              <div key={index}>
-                <Card data={data} />;
-              </div>
-            );
-          })
-        ) : (
-          <div className="w-screen flex justify-center items-center h-1/2">
-            <p className="text-2xl text-neutral-700">
-              You dont have any wishlist
-            </p>
-          </div>
-        )
-      ) : (
-        <div className="w-screen flex justify-center items-center h-1/2">
-          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-neutral-900"></div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{!isLoading ? (
+				listAuction.length > 0 ? (
+					listAuction.map((data, index) => {
+						return (
+							<div key={index}>
+								<Card data={data} />;
+							</div>
+						);
+					})
+				) : (
+					<div className="w-screen flex justify-center items-center h-1/2">
+						<p className="text-2xl text-neutral-700">You dont have any wishlist</p>
+					</div>
+				)
+			) : (
+				<div className="w-screen flex justify-center items-center h-1/2">
+					<div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-neutral-900"></div>
+				</div>
+			)}
+		</>
+	);
 }
